9b4514397e2fae9e93bea0da6cf2c8ca71c471d8
Jonne Vuorela <vuorelajonne@gmail.com>
2024-03-02 17:38:14 +0200

    svelte

--- svelte.config.js
+++
@@ -1,9 +0,0 @@
-import adapter from '@sveltejs/adapter-static';
-//export default config;
-export default {
-kit: {
-adapter: adapter({
-fallback: 'index.html' // may differ from host to host
-})
-}
-};

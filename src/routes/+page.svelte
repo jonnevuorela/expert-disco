afce041000643e60f332f3b98804a506c204c23f
Jonne Vuorela <vuorelajonne@gmail.com>
2024-03-02 17:00:01 +0200

    svelte1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    import Hobbies from "./Hobbies.svelte";
+    import AlvLaskuri from "./AlvLaskuri.svelte";
+    import Tuotelaskuri from "./Tuotelaskuri.svelte";
+    import Kauppalista from "./kauppalista.svelte";
+    import Router, {push} from "svelte-spa-router";
+    let name ="world";
+    const routes ={
+        "/": Hobbies,
+        "/alv": AlvLaskuri,
+        "/tuotelaskuri": Tuotelaskuri,
+        "/kauppalista": Kauppalista
+    }
+</script>
+
+<nav>
+    <a href="/#/">Home</a>
+    <a href="/#/alv">AlvLaskuri</a>
+    <a href="/#/tuotelaskuri">Tuotelaskuri</a>
+    <a href="/#/kauppalista">Kauppalista</a>
+</nav>
+
+<h1 style="font-family: monospace;">
+    Hello {name.toUpperCase()}!
+</h1>
+
+<p>Tervetuloa yritykseni sivuille</p>
+
+<Hobbies/>
+
+<style>
+    a {
+        padding: 10px;
+        background-color: aqua;
+        border-radius: 5px;
+        border-color: aquamarine;
+        
+    }
+    p{
+        color:red;
+    }
+</style>
+
+<Router {routes}/>

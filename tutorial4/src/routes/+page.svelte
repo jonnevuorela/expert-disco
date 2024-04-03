752b46a8c3d6d49ba49b008ad17b9d6a3d228bbe
Jonne <vuorelajonne@gmail.com>
2024-04-03 14:09:00 +0300

    tutorial4

---
+++ tutorial4/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import Employees from "./Employees.svelte";
+    import Edit from "./Edit.svelte";
+    import Router, { push } from "svelte-spa-router";
+
+    const routes = {
+        "/": Employees,
+        "/employees": Employees,
+        "/edit": Edit,
+    };
+</script>
+
+<nav>
+    <a href="/#/employees">Employees</a>
+    <a href="/#/edit">Edit</a>
+</nav>
+<Router {routes} />
+
+<style>
+    nav {
+        display: flex;
+        gap: 1rem;
+    }
+
+    a {
+        text-decoration: none;
+        background-color: navajowhite;
+        padding: 1em;
+        border-radius: 0.2em;
+        box-shadow: 5px 5px 15px black;
+    }
+</style>
---
+++ tutorial4/src/routes/Employees.svelte
@@ -0,0 +1,51 @@
+<script>
+    let employees = [];
+    fetch("http://localhost:5000/employees")
+        .then((response) => response.json())
+        .then((json) => {
+            employees = json;
+        });
+</script>
+
+<h1>Employees</h1>
+{#if employees.length}
+    <table>
+        <tr>
+            <th>Emp no</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+        </tr>
+        {#each employees as emp}
+            <tr>
+                <td>{emp.employeeNumber}</td>
+                <td>{emp.firstName}</td>
+                <td>{emp.lastName}</td>
+            </tr>
+        {/each}
+    </table>
+{/if}
+
+<style>
+    h1 {
+        font-family: "Arial", sans-serif;
+        text-align: center;
+        color: black;
+        border: 1px solid black;
+        box-shadow: 5px 5px 15px black;
+        background-color: moccasin;
+    }
+    table {
+        border-collapse: collapse;
+        background-color: goldenrod;
+        box-shadow: 5px 5px 15px black;
+        width: 100%;
+    }
+    tr,
+    th,
+    td {
+        border: 1px solid black;
+    }
+    tr:nth-child(even) {
+        background-color: moccasin;
+    }
+</style>
